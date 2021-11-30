014fbf4ec3cd8ad96575cd7654e96545ed9117d7
KhFazliddin <81314108+KhFazliddin@users.noreply.github.com>
2021-11-30 19:48:55 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
 import logo from './logo.svg';
-import './App.css';
+import './assets/style/style.css'
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Todoitem from './components/Todoitem';
+import Filter from './components/Filter';
+import todo from './store/reducer/todo';
+import { connect } from "react-redux";
+import { completeTodo, completeAll, deleteTodo } from './store/action/todo';
 
-function App() {
+function App({ todos, deleteTodo, completeAll, completeTodo, filter }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <body>
+      <section class="todoapp">
+        <Header />
+        <section class="main">
+          <input id="toggle-all" class="toggle-all" type="checkbox" />
+          <label onClick={completeAll} for="toggle-all">Mark all as complete</label>
+          <ul class="todo-list">
+            {
+              todos.map(todo => {
 
-export default App;
+                const item = <Todoitem
+                  title={todo.title}
+                  completed={todo.completed}
+                  deleteTodo={() => deleteTodo(todo.id)}
+                  completeTodo={() => {
+                    completeTodo(todo.id)
+                  }}
+                />
+
+                if (filter === 'active' && todo.completed === false) {
+                  return item
+                } else if (filter === 'completed' && todo.completed === true) {
+                  return item
+                } else if (filter === 'all') {
+                  return item;
+                }
+              }
+              )
+            }
+          </ul>
+          <Filter />
+        </section>
+      </section>
+      <Footer />
+    </body>
+  );
+};
+
+const mapStateToProps = (state) => ({
+  todos: state.todo.todos,
+  filter: state.todo.filter
+});
+
+const mapDispatchToProps = ({
+  deleteTodo,
+  completeAll,
+  completeTodo
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
---
+++ src/store/store.js
@@ -0,0 +1,13 @@
+import { combineReducers, compose, createStore } from "redux";
+import { composeWithDevTools } from 'redux-devtools-extension';
+import todo from "./reducer/todo";
+
+
+const rootReducer = combineReducers({
+    todo,
+});
+
+
+const store = createStore(rootReducer, composeWithDevTools());
+
+export default store;
